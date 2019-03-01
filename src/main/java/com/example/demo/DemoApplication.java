368dbd344411c4aff5234194ef873250293b0068
Dan Dentinger <dan.dentinger@kroger.com>
2019-03-01 09:36:29 -0500

    Made subscription a property as well as project id

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -1,8 +1,10 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 import org.springframework.integration.channel.DirectChannel;
 import org.springframework.messaging.MessageChannel;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -12,33 +14,34 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.integration.annotation.ServiceActivator;
 
-
 @SpringBootApplication
 public class DemoApplication {
 
-	private static final Log LOGGER = LogFactory.getLog(DemoApplication.class);
+  private static final Log LOGGER = LogFactory.getLog(DemoApplication.class);
 
-        public static void main(String[] args) {
-                SpringApplication.run(DemoApplication.class, args);
-        }
+  @Autowired private Environment env;
 
-        @Bean
-        public MessageChannel pubsubInputChannel() {
-                return new DirectChannel();
-        }
+  public static void main(String[] args) {
+    SpringApplication.run(DemoApplication.class, args);
+  }
 
-        @Bean
-        public PubSubInboundChannelAdapter messageChannelAdapter(
-                        @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
-                        PubSubTemplate pubSubTemplate) {
-                PubSubInboundChannelAdapter adapter =
-                                new PubSubInboundChannelAdapter(pubSubTemplate, "test_read");
-                adapter.setOutputChannel(inputChannel);
-                return adapter;
-        }
+  @Bean
+  public MessageChannel pubsubInputChannel() {
+    return new DirectChannel();
+  }
 
-        @ServiceActivator(inputChannel = "pubsubInputChannel")
-        public void messageReceiver(String payload) {
-                LOGGER.info("Message arrived! Payload: " + payload);
-        }
+  @Bean
+  public PubSubInboundChannelAdapter messageChannelAdapter(
+      @Qualifier("pubsubInputChannel") MessageChannel inputChannel,
+      PubSubTemplate pubSubTemplate) {
+    PubSubInboundChannelAdapter adapter =
+        new PubSubInboundChannelAdapter(pubSubTemplate, env.getProperty("subscription.name", "test_read"));
+    adapter.setOutputChannel(inputChannel);
+    return adapter;
+  }
+
+  @ServiceActivator(inputChannel = "pubsubInputChannel")
+  public void messageReceiver(String payload) {
+    LOGGER.info("Message arrived! Payload: " + payload);
+  }
 }
